a0588a082031e20e00f67793a18bf7eca83bd6d6
Your Name <you@example.com>
2021-03-12 07:23:57 -0600

    commit2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import GoogleMap from './components/GoogleMap';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
       <h1>Google Maps App</h1>
       <GoogleMap />
+
+      <Helmet>
+<script defer data-key="d756976a-4e8d-457f-8556-068d25d2d226" src="https://widget.tochat.be/bundle.js"></script>
+       </Helmet>
     </div>
+    
   );
 }
+
+
 
 export default App;
